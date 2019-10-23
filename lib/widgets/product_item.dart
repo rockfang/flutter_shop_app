683f451e593d4336b00a15f0639f1ed8baa1332d
caifang <cooldish1245@163.com>
2019-10-23 10:13:42 +0800

    test dev commit

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -8,7 +8,8 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child: Image.network(imageUrl),
+      child: Image.network(imageUrl,fit: BoxFit.cover,),
+      footer: GridTileBar(backgroundColor: Colors.black54,title: Text("名称"),),
     );
   }
 }
